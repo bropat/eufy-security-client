be1a82bbdb6121a1fa7c5c0c18206c595e1f8cd1
Philipp Engler <77169354+PhilippEngler@users.noreply.github.com>
2023-09-07 15:48:54 +0200

    changed StatusLed property for doorbell

--- src/http/types.ts
+++ src/http/types.ts
@@ -1153,8 +1153,17 @@ export const DeviceStatusLedIndoorFloodProperty: PropertyMetadataBoolean = {
 };
 
 export const DeviceStatusLedBatteryDoorbellProperty: PropertyMetadataBoolean = {
-    ...DeviceStatusLedProperty,
     key: CommandType.CMD_BAT_DOORBELL_SET_LED_ENABLE,
+    name: PropertyName.DeviceStatusLed,
+    label: "Status LED",
+    readable: true,
+    writeable: true,
+    type: "number",
+    states: {
+        0: "Off",
+        1: "All day",
+        2: "At night",
+    },
 };
 
 export const DeviceStatusLedDoorbellProperty: PropertyMetadataBoolean = {
