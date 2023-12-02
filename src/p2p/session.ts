1254cc820e7a9b4b8a2f20cea7b27ef1c48a480f
bropat <patrick.broetto@gmail.com>
2023-12-02 10:38:58 +0100

    Lowered p2p keepalive interval for battery saving devices

--- src/p2p/session.ts
+++ src/p2p/session.ts
@@ -35,7 +35,7 @@ export class P2PClientProtocol extends TypedEmitter<P2PClientProtocolEvents> {
     private readonly HEARTBEAT_INTERVAL = 5 * 1000;
     private readonly MAX_COMMAND_QUEUE_TIMEOUT = 120 * 1000;
     private readonly AUDIO_CODEC_ANALYZE_TIMEOUT = 650;
-    private readonly KEEPALIVE_INTERVAL = 5 * 1000;
+    private readonly KEEPALIVE_INTERVAL = 2 * 1000;
     private readonly ESD_DISCONNECT_TIMEOUT = 30 * 1000;
     private readonly MAX_STREAM_DATA_WAIT = 5 * 1000;
     private readonly RESEND_NOT_ACKNOWLEDGED_COMMAND = 100;
