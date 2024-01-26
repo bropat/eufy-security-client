155c64a628450aa931cb9d0e756ba1e60d6b70d2
Martijn Poppen <m.poppen@ziggo.nl>
2024-01-26 10:57:41 +0100

    FIX: vehicle detection S340

--- src/http/types.ts
+++ src/http/types.ts
@@ -6320,6 +6320,7 @@ export const DeviceProperties: Properties = {
         [PropertyName.DeviceWatermark]: DeviceWatermarkProperty,
         [PropertyName.DeviceMotionDetected]: DeviceMotionDetectedProperty,
         [PropertyName.DevicePersonDetected]: DevicePersonDetectedProperty,
+        [PropertyName.DeviceVehicleDetected]: DeviceVehicleDetectedProperty,
         [PropertyName.DeviceStatusLed]: DeviceStatusLedIndoorFloodProperty,
         [PropertyName.DeviceLight]: DeviceFloodlightLightProperty,
         [PropertyName.DeviceLightSettingsBrightnessManual]: DeviceFloodlightLightSettingsBrightnessManualProperty,
