7448306011d15f3697693bf6b0fb2cfbb232a268
christian bianchini <max246@gmail.com>
2025-12-15 16:24:05 +0000

    fix compile issues

--- src/p2p/utils.ts
+++ src/p2p/utils.ts
@@ -261,7 +261,7 @@ export const buildCommandHeader = (seqNumber: number, commandType: CommandType, 
         Buffer.compare(p2pDataTypeHeader, P2PDataTypeHeader.BINARY) === 0 ||
         Buffer.compare(p2pDataTypeHeader, P2PDataTypeHeader.CONTROL) === 0 ||
         Buffer.compare(p2pDataTypeHeader, P2PDataTypeHeader.VIDEO) === 0)) {
-        dataTypeBuffer = p2pDataTypeHeader;
+        dataTypeBuffer =  p2pDataTypeHeader as Buffer<ArrayBuffer>;
     }
     const seqAsBuffer = Buffer.allocUnsafe(2);
     seqAsBuffer.writeUInt16BE(seqNumber, 0);
